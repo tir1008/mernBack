a97c06c5a7ec44ccebb12b425e369c38b7ce86f4
Tirthesh Pancholi <tirtheshpancholi@gmail.com>
2024-03-28 17:56:31 +0530

    scommit

--- index.js
+++ index.js
@@ -4,26 +4,30 @@ global.foodData = require('./db')(function call(err, data, CatData) {
   global.foodData = data;
   global.foodCategory = CatData;
 })
+const express = require('express');
+const app = express();
+const port = 5000;
 
-const express = require('express')
-const app = express()
-const port = 5000
+// Middleware for handling CORS requests from your front-end domain
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  // Replace 'http://localhost:3000' with the domain of your front-end application
+  res.setHeader('Access-Control-Allow-Origin', 'https://mernfront-gmgo.onrender.com');
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
-app.use(express.json())
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.use('/api/auth', require('./Routes/Auth'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`);
+});
